616a2fecd28406a92372fd52e24d594bcdeef88b
Gabriela <71526049+Gabriela22204@users.noreply.github.com>
2022-06-23 22:05:03 -0300

    new funcion
    feat: new function add to display rover on plato

--- src/App.java
+++ src/App.java
@@ -6,6 +6,21 @@ public class App {
         OESTE
     };    
 
+    public static void mostra(Rover robo, Plato plato){       
+        for(int i = 0; i <= plato.limiteX; i++){ //linhas
+            for(int j = 0; j <= plato.limiteY; j++){ //colunas
+                if(-1*(i - plato.limiteX) == robo.y && j == robo.x){
+                    System.out.print("R\t");
+                }
+                else{
+                    System.out.print(".\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("O robo esta em " + robo.x + " e em " + robo.y + ", olhando para o " + robo.direcao);
+    }
+
     public static void main(String[] args) throws Exception {
         InputReader input = new InputReader();
         Plato plato = input.retornaPlato();
@@ -15,6 +30,8 @@ public class App {
             char[] comandos = input.retornaComandos();
             for (int i = 0; i < comandos.length; i++){
                 rover.decifraComando(comandos[i], plato.limiteX, plato.limiteY);
+                
+                mostra(rover, plato);
             }
             System.out.println("POSICAO FINAL: " + rover.x + " , " + rover.y + " , " + rover.direcao);
         }
